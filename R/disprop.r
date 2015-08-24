4125dff32047baf07c2f29bcfcb03e5653515554
Dmitri Pervouchine <pervouchine@gmail.com>
2015-08-24 16:17:34 +0200

    v3.2

--- R/disprop.r
+++ R/disprop.r
@@ -10,7 +10,7 @@ for(i in 6:(length(cmd_args)-1)) {
     a$id  = unlist(lapply(strsplit(as.character(a$V1),"_"),function(x){paste(x[1:3],collapse="_")}))
     a$str = unlist(lapply(strsplit(as.character(a$V1),"_"),function(x){x[4]})) 
     with(a, aggregate(list(count=V2),by=list(id=id),FUN=function(x){log10(x[2])-log10(x[1])})) -> z
-    hist(z$count, breaks=100) -> h
+    hist(z$count, breaks=20) -> h
     b = data.frame(file=cmd_args[i], mids = h$mids, density = h$density)
     df = rbind(df, b)
 }
@@ -20,6 +20,6 @@ gsub(".A03.ssj.tsv","", x) -> df$sample
 
 pdf(pdf)
 theme_set(theme_bw(base_size = 12))
-ggplot(df,aes(x=mids,y=density,colour=sample)) + geom_line() + geom_point() + xlab("log10(+) - log10(-)")
+ggplot(df,aes(x=mids,y=density,colour=sample)) + geom_line() + geom_point() + xlab("log10(c+) - log10(c-)")
 dev.off()
 
